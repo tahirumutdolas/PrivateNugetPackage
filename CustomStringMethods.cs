2c57715a4b22a37cea8c7d038515fafc03f38453
umut.dolas <umut.dolas@enuygun.com>
2023-09-25 11:25:33 +0300

    Add project files.

---
+++ CustomStringMethods.cs
@@ -0,0 +1,17 @@
+﻿
+public static class CustomStringMethods
+{
+    // Özel bir metot örneği: Bir metin içinde belirli bir alt metni arama
+    public static bool ContainsSubstring(this string str, string substring)
+    {
+        return str.Contains(substring);
+    }
+
+    // Özel bir metot örneği: Metindeki boşlukları kaldırma
+    public static string RemoveSpaces(this string str)
+    {
+        return str.Replace(" ", string.Empty);
+    }
+
+    // Daha fazla özel metot ekleyebilirsiniz
+}
